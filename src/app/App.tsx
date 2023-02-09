d02a9fc38aac8a4ae165e7dbf9f69e0b8e825f98
alinovskiy <0885466@gmail.com>
2023-02-09 21:42:05 +0700

    [PROF-14] feat: add sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,21 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 
 const App = () => {
-   const { theme } = useTheme();
+  const {theme} = useTheme();
 
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <AppRouter />
-        </div>
-    );
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <Navbar/>
+      <div className="content-page">
+        <Sidebar/>
+        <AppRouter/>
+      </div>
+    </div>
+  );
 };
 
 export default App;
