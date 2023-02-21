42a2c5f998664dc7227207ad5aa966f6df239b78
alinovskiy <0885466@gmail.com>
2023-02-21 12:33:07 +0700

    [FSD-30] feat: add redux add test for counter component

--- src/app/providers/Theme-provider/lib/use-theme.ts
+++ src/app/providers/Theme-provider/lib/use-theme.ts
@@ -9,9 +9,6 @@ type UseThemeResult = {
 export const useTheme = (): UseThemeResult => {
     const { theme, setTheme } = useContext(ThemeContext);
 
-    console.log(typeof setTheme);
-    console.log(theme);
-
     const toggleTheme = () => {
         const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Counter } from 'entity/Counter/ui/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
