4c879f5de2d8c1e765b344d7b7351bea20c2a573
alinovskiy <0885466@gmail.com>
2023-06-19 17:10:58 +0700

    [FSD-64] feat: add eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
         '@typescript-eslint',
         'i18next',
         'react-hooks',
+        'alinovskiy-plugin-fsd',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -56,6 +57,7 @@ module.exports = {
         'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
         'no-param-reassign': 'off',
         'no-undef': 'off',
+        'alinovskiy-plugin-fsd/path-checker': 'error',
     },
     globals: {
         __IS_DEV__: true,
