76617edc73ad2aa332d4450dc6f845fdcbfcec77
alinovskiy <0885466@gmail.com>
2023-06-21 11:38:49 +0700

    [FSD-70] feat: add rtkQuery

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,7 +47,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['data-testid', 'to', 'role', 'as', 'direction'],
+                ignoreAttribute: ['data-testid', 'to', 'role', 'as', 'direction', 'target', 'justify'],
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 120 }],
