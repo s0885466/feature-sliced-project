3e099387930527090f7b832305fb1dc365a3e5cd
alinovskiy <0885466@gmail.com>
2023-06-23 15:28:45 +0700

    [FSD-97] feat: add documentation

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,13 +1,9 @@
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
-// eslint-disable-next-line alinovskiy-plugin-fsd/layer-imports
 import { loginReducer } from '@/features/AuthByUsername/testing';
 import { ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-// eslint-disable-next-line alinovskiy-plugin-fsd/layer-imports
 import { articleDetailsReducer } from '@/entities/Article/testing';
-// eslint-disable-next-line alinovskiy-plugin-fsd/layer-imports
 import { addCommentFormReducer } from '@/features/addCommentForm/testing';
-// eslint-disable-next-line alinovskiy-plugin-fsd/layer-imports
 import { profileReducer } from '@/features/editableProfileCard/testing';
 
 const defaultAsyncReducers: ReducersList = {
