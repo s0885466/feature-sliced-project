95c945760e9462e7f644181b3425ca1cc7bcbe9a
alinovskiy <0885466@gmail.com>
2023-06-23 11:26:31 +0700

    [FSD-91] feat: update eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -59,10 +59,13 @@ module.exports = {
         'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
         'no-param-reassign': 'off',
         'no-undef': 'off',
-        'alinovskiy-plugin-fsd/path-checker': ['error', { alias: '@' }],
-        'alinovskiy-plugin-fsd/public-api-imports': ['error', { alias: '@' }],
         'react/no-array-index-key': 'warn',
         'arrow-body-style': 'off',
+        'alinovskiy-plugin-fsd/path-checker': ['error', { alias: '@' }],
+        'alinovskiy-plugin-fsd/public-api-imports': ['error', {
+            alias: '@',
+            testFilesPatterns: ['**/*.test.*', '**/*.story.*', '**/StoreDecorator.tsx'],
+        }],
     },
     globals: {
         __IS_DEV__: true,
